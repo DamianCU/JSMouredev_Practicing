afdc775670d711e3c44a1b080d3e044192e6af73
DamianCU <158050732+DamianCU@users.noreply.github.com>
2025-02-06 23:31:41 +0100

    3:18:00 Bucles

---
+++ 14 - Bucles.js
@@ -0,0 +1,14 @@
+//BUCLES 
+
+     /*
+      Los bucles en JavaScript son estructuras que permiten ejecutar un bloque de código varias veces de forma automática, lo que evita la repetición manual y mejora la eficiencia del código.
+     */
+    
+     // For
+        // Se ejecuta en cuanto lo lanzamos y tiene una condicion que se evalua cada iteración.
+
+        for(let i=0; i<5; i++) {
+            console.log(`Hola ${i}`);         
+        }
+
+        const numbers =[1,2,3,4,5,6,7]
