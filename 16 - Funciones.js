55aa6cf6196d30eef052ab2fdd5308e573dfad69
DamianCU <158050732+DamianCU@users.noreply.github.com>
2025-02-13 13:24:17 +0100

    practicing

---
+++ 16 - Funciones.js
@@ -0,0 +1,23 @@
+// FUNCIONES
+
+    // SIMPLE
+    function myFunction(){
+        console.log("Hola, función");
+    }
+    myFunction();
+
+    for (let i = 0; i<5 ; i++){
+        myFunction();
+    }
+
+    // CON PARÁMETROS
+    function myFunctionParams(name){
+        console.log(`Hola, ${name}!`);        
+    }
+    myFunctionParams("Damián");
+    myFunctionParams("Carrillo");
+
+
+    // FUNCIONES ANÓNIMAS
+
+    
