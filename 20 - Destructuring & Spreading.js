8662aab73dd2a519dc4efabe0d1b420d7e978aca
DamianCU <158050732+DamianCU@users.noreply.github.com>
2025-02-21 20:40:42 +0100

    Ejercicios objetos

---
+++ 20 - Destructuring & Spreading.js
@@ -0,0 +1,23 @@
+
+//TODO: DESESTRUCTURACIÓN
+
+      //* ES UNA SINTÁXIS QUE NOS PERMITE EXTRAER VALORES DE ARRAYS POR UN LADO O DE OBJETOS Y ASIGNARLOS A VARIABLES DE MANERA CONCISA
+
+      let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+      let person = {
+        name: "Damián",
+        surname: "Carrillo",
+        age: 35,
+        email: "dcu191089@hotmail.es"
+      }
+
+      let myValue = myArray[1]
+      console.log(myValue);
+
+
+    //? DESESTRUCTURACIÓN
+      //* Sintáxis arrays
+      
+
+      
