e41549606a5160360e524d6e8fa9339627923c49
DamianCU <158050732+DamianCU@users.noreply.github.com>
2025-02-25 01:34:14 +0100

    Destruct & Spreading

---
+++ 21 - Exercises-Destruct&Spreading.js
@@ -0,0 +1,10 @@
+//? 1. Usa desestructuracion para extraer los dos primeros elementos de un array
+//? 2. Usa desestructuracion en un array y asigna un valor predeterminado a una variable
+//? 3. Usa desestructuracion para extraer dos propiedades de un objeto
+//? 4. Usa desestructuracion para extraer dos propiedades de un objeto y asignarles a nuevas variables con nombres diferentes
+//? 5. Usa desestructuracion para extraer dos propiedades de un objeto anidado
+//? 6. Usa propagacion para combinar dos arrays en uno nuevo
+//? 7. Usa propagación para crear una copia de un array
+//? 8. Usa propagación para combinar dos objetos en uno nuevo
+//? 9. Usa propagación para crear una copia de un objeto
+//? 10. Combina desestructuración y propagación 
